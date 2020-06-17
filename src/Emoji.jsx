7c86d9bce66a015f3b37dfcf764b0d7194edc658
Nicolás Gómez <nicolasgomezespejo@gmail.com>
2020-06-17 11:39:39 -0400

    use svg emojis

--- src/Emoji.jsx
+++ src/Emoji.jsx
@@ -3,7 +3,7 @@ import Twemoji from "react-twemoji";
 
 export default function Emoji({ value }) {
   return (
-    <Twemoji tag="span" options={{ className: "emoji" }}>
+    <Twemoji tag="span" options={{ className: "emoji", ext: ".svg", folder: "svg" }}>
       {value}
     </Twemoji>
   );
