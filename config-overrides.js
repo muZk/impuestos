bbc3038b45e131e998b4407028fcd404021eca12
Nicolás Gómez <nicolasgomezespejo@gmail.com>
2020-06-20 04:15:03 -0400

    adds splash

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,28 @@
 const rewireCompressionPlugin = require('react-app-rewire-compression-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = function override(config, env) {
   config = rewireCompressionPlugin(config, env, {
     test: /\.js(\?.*)?$/i,
     cache: true
+  });
+
+  config.plugins.push(new MiniCssExtractPlugin());
+
+  let loaders = config.module.rules[2].oneOf;
+  loaders.splice(loaders.length - 1, 0, {
+    test: /\.(sa|sc|c)ss$/,
+    use: [
+      {
+        loader: MiniCssExtractPlugin.loader,
+        options: {
+          hmr: process.env.NODE_ENV === 'development',
+        },
+      },
+      'css-loader',
+      'postcss-loader',
+      'sass-loader',
+    ],
   });
 
   return config;
