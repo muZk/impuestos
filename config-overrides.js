6d0d06b421619b63ea022279767993125bd2801d
Nicolás Gómez <nicolasgomezespejo@gmail.com>
2020-06-20 15:16:31 -0400

    fixes inserting css twice

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,6 @@
-const rewireCompressionPlugin = require('react-app-rewire-compression-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = function override(config, env) {
-  config = rewireCompressionPlugin(config, env, {
-    test: /\.js(\?.*)?$/i,
-    cache: true
-  });
-
-  config.plugins.push(new MiniCssExtractPlugin());
-
   let loaders = config.module.rules[2].oneOf;
   loaders.splice(loaders.length - 1, 0, {
     test: /\.(sa|sc|c)ss$/,
@@ -20,7 +12,6 @@ module.exports = function override(config, env) {
         },
       },
       'css-loader',
-      'postcss-loader',
       'sass-loader',
     ],
   });
