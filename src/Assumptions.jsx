7f6d42983941428ff2e4ab3ab4a351ff706a929a
Nicolás Gómez <ngomez@hey.com>
2024-02-22 09:42:56 -0300

    ✨ update details info (#43)

--- src/Assumptions.jsx
+++ src/Assumptions.jsx
@@ -1,31 +1,35 @@
 import React from "react";
-import { obtenerConfiguracion, COTIZACIONES_OBLIGATORIAS } from "tax-cl";
+import { obtenerConfiguracion } from "tax-cl";
 import Amount from "./Amount";
 
 export default function Assumptions() {
   const { UF, OPERACION_RENTA } = obtenerConfiguracion();
-  const { percent: AFP } = COTIZACIONES_OBLIGATORIAS.find(({ name }) => name === "AFP");
 
   return (
     <>
       <h2>Supuestos para el cálculo:</h2>
       <ul>
         <li>
-          Los cálculos considera valores para la <strong>Operación Renta {OPERACION_RENTA}</strong>
+          Los cálculos considera valores para la{" "}
+          <strong>Operación Renta {OPERACION_RENTA}</strong> (considera tus
+          ingresos del año {OPERACION_RENTA - 1})
         </li>
         <li>
           Valor UF: <Amount value={UF} /> (31 diciembre {OPERACION_RENTA - 1})
         </li>
         <li>Eres trabajador independiente</li>
-        <li>Estás en el tramo etario que paga retenciones.</li>
+        <li>
+          Tienes la obligación a cotizar (hay personas que no deben hacerlo).
+        </li>
         <li>
           No tienes otras fuentes de ingreso (inversiones, sociedades,
           viviendas, etc).
         </li>
         <li>No tienes APV-B.</li>
-        <li>{(AFP - 10).toFixed(2)}% es la comisión de tu AFP.</li>
+        <li>El porcentaje de la AFP incluye la comisión.</li>
         <li>
-          Tus gastos se calculan en base a "gastos supuestos" (30% de tu bruto)
+          Tus gastos se calculan en base a "gastos presuntos" (30% de tu bruto,
+          hasta un máximo de 15 UTA)
         </li>
       </ul>
     </>
--- src/Details.jsx
+++ src/Details.jsx
@@ -9,15 +9,11 @@ import Amount from "./Amount";
 import Assumptions from "./Assumptions";
 import Emoji from "./Emoji";
 
-const {
-  TRAMOS_IMPOSITIVOS,
-  TOPE_IMPONIBLE_MENSUAL,
-  RETENCION,
-} = obtenerConfiguracion();
-
-const TOPE_IMPONIBLE_ANUAL_EN_UF = TOPE_IMPONIBLE_MENSUAL * 12;
-
 export default function Details({ result }) {
+  const { TRAMOS_IMPOSITIVOS, TOPE_IMPONIBLE_MENSUAL, RETENCION, UF } =
+    obtenerConfiguracion();
+
+  const TOPE_IMPONIBLE_ANUAL_EN_UF = TOPE_IMPONIBLE_MENSUAL * 12;
   const {
     sueldoAnual,
     gastos,
@@ -29,6 +25,8 @@ export default function Details({ result }) {
     cotizaciones,
   } = result;
   const activeStep = buscarTramoImpositivo(sueldoTributable);
+  const imponibleCotizaciones = calcularSueldoImponible(sueldoAnual);
+  const maximoImponible = UF * TOPE_IMPONIBLE_ANUAL_EN_UF;
   return (
     <div className="dark">
       <section>
@@ -39,15 +37,17 @@ export default function Details({ result }) {
           Tu <strong>ingreso bruto anual</strong> es {formatAmount(sueldoAnual)}
         </p>
         <p>
-          A tu favor, tienes la <strong>retención de tus boletas</strong> (el {100 * RETENCION}%), lo cual es <Amount success value={retencion} />
+          A tu favor, tienes la <strong>retención de tus boletas</strong> (el{" "}
+          {100 * RETENCION}%), lo cual es <Amount success value={retencion} />
         </p>
         <blockquote>
-          Esta retención se usa para pagar impuestos y obligaciones legales
-          tales como AFP y salud (fonasa o isapre).
+          Esta retención se usa para pagar impuestos y cotizaciones tales como
+          AFP y salud (fonasa o isapre).
         </blockquote>
         <p>
-          En <i>contra</i> tienes los impuestos (<Amount danger value={impuestos} />
-          ) y las obligaciones legales (
+          En <i>contra</i> tienes los impuestos (
+          <Amount danger value={impuestos} />
+          ) y las cotizaciones (
           <Amount danger value={montoCotizacionesObligatorias} />)
         </p>
         <h3>Cálculo del impuesto</h3>
@@ -61,8 +61,8 @@ export default function Details({ result }) {
             <strong>Gastos presuntos</strong>
           </li>
           <li>
-            Gastos presuntos = <Amount value={gastos} /> (30% de tu BRUTO
-            hasta un máximo de 15UTA)
+            Gastos presuntos = <Amount value={gastos} /> (30% de tu BRUTO hasta
+            un máximo de 15UTA)
           </li>
         </ul>
         <p>
@@ -84,12 +84,17 @@ export default function Details({ result }) {
           </thead>
           <tbody>
             {TRAMOS_IMPOSITIVOS.map((step, index) => {
-              const from = index === 0 ? 0 : TRAMOS_IMPOSITIVOS[index - 1].montoMaximo + 0.01;
+              const from =
+                index === 0
+                  ? 0
+                  : TRAMOS_IMPOSITIVOS[index - 1].montoMaximo + 0.01;
               const to = step.montoMaximo;
               return (
                 <tr
                   className={
-                    sueldoTributable > from && sueldoTributable < to ? "active" : ""
+                    sueldoTributable > from && sueldoTributable < to
+                      ? "active"
+                      : ""
                   }
                   key={`step-${index}`}
                 >
@@ -113,19 +118,27 @@ export default function Details({ result }) {
           {activeStep.factor} x {formatAmount(sueldoTributable)} -{" "}
           {formatAmount(activeStep.descuento)} = {formatAmount(impuestos)}
         </code>
-        <h3>Descuentos Legales</h3>
+        <h3>Cotizaciones</h3>
         <p>
-          Los descuentos legales se calculan sobre tu{" "}
-          <strong>Bruto Imponible</strong> el cual es igual al 80% de tu bruto,
-          hasta un máximo de {TOPE_IMPONIBLE_ANUAL_EN_UF.toFixed(2)} (UF).
+          Las cotizaciones se calculan sobre tu <strong>Bruto Imponible</strong>{" "}
+          el cual es igual al 80% de tu bruto, hasta un máximo de{" "}
+          {TOPE_IMPONIBLE_ANUAL_EN_UF.toFixed(2)} UF (aproximadamente{" "}
+          <Amount value={maximoImponible} />
+          ).
+        </p>
+        <p>
+          {imponibleCotizaciones > maximoImponible
+            ? "Como tu bruto imponible es mayor al tope, se considera el tope como base para el cálculo de las cotizaciones."
+            : "Como tu bruto imponible es menor al tope, se considera tu bruto imponible como base para el cálculo de las cotizaciones."}
         </p>
         <p>
           Por lo tanto, <strong>Bruto Imponible</strong> ={" "}
-          <Amount value={calcularSueldoImponible(sueldoAnual)} />
+          <Amount value={imponibleCotizaciones} />
         </p>
         <p>
-          Tu monto total de descuento legal es{" "}
-          <Amount value={montoCotizacionesObligatorias} />. A continuación el detalle:
+          El monto total que se va a tus cotizaciones es{" "}
+          <Amount value={montoCotizacionesObligatorias} />. A continuación el
+          detalle:
         </p>
         <table>
           <thead>
@@ -141,12 +154,18 @@ export default function Details({ result }) {
                 <td>{name}</td>
                 <td>{Number(percent.toFixed(2))}%</td>
                 <td>
-                  {formatAmount((calcularSueldoImponible(sueldoAnual) * percent) / 100)}
+                  {formatAmount(
+                    (calcularSueldoImponible(sueldoAnual) * percent) / 100
+                  )}
                 </td>
               </tr>
             ))}
           </tbody>
         </table>
+        <blockquote>
+          Nota: La AFP se paga con el remanente de tu retención. Es por eso que
+          el porcentaje de cotización es menor al 10%.
+        </blockquote>
         <h3>Cálculo Final</h3>
         Resumiendo tu situación:
         <ul>
@@ -158,7 +177,7 @@ export default function Details({ result }) {
             Tienes que pagar impuestos: <Amount danger value={impuestos} />
           </li>
           <li>
-            Tienes que pagar obligaciones legales:{" "}
+            Tienes que pagar cotizaciones:{" "}
             <Amount danger value={montoCotizacionesObligatorias} />
           </li>
         </ul>
@@ -166,7 +185,8 @@ export default function Details({ result }) {
           Deuda ={" "}
           <code>
             {formatAmount(montoCotizacionesObligatorias)} +{" "}
-            {formatAmount(impuestos)} - {formatAmount(retencion)} = {formatAmount(deuda)}
+            {formatAmount(impuestos)} - {formatAmount(retencion)} ={" "}
+            {formatAmount(deuda)}
           </code>
         </p>
         <p>
